8486190e6c35987e640b6f6782b18f5a8469c68b
Shane <shanejung12@gmail.com>
2019-09-24 21:52:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bandaid/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bandaid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start_screen)
+    }
+}
---
+++ app/src/main/java/com/example/bandaid/Start.kt
@@ -0,0 +1,11 @@
+package com.example.bandaid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Start :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start_screen)
+    }
+}
