74aa81fb9f239656b417ee502c8c0669ddd3fb60
Shane <shanejung12@gmail.com>
2019-09-26 12:25:58 -0500

    All modifications

--- app/src/main/java/com/example/bandaid/CreateAccount.kt
+++ app/src/main/java/com/example/bandaid/CreateAccount.kt
@@ -1,11 +1,18 @@
 package com.example.bandaid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class Start :AppCompatActivity(){
+class CreateAccount :AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_account1)
+
+    }
+    fun aboutYou(view: View)
+    {
+        setContentView(R.layout.create_account2)
     }
 }
--- app/src/main/java/com/example/bandaid/MainActivity.kt
+++ app/src/main/java/com/example/bandaid/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.bandaid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
+import android.view.View
+import kotlinx.android.synthetic.main.start_screen.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_screen)
+
     }
+    fun buttonPress(view: View)
+    {
+        startActivity(Intent(this, CreateAccount::class.java))
+    }
+
 }
